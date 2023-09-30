c8c5e19ac1f7ca44701235bcf88533a44a2cea61
stclamp <whatisclamp@gmail.com>
2023-09-30 14:30:21 +0300

    feat: add enum for messages. add trycatch for controller

--- src/controller/post.controller.ts
+++ src/controller/post.controller.ts
@@ -1,52 +1,83 @@
 import { RequestHandler } from 'express';
 import { Post } from '../models/post.model';
+import { EPostMessages } from 'src/types';
 
 export const createPost: RequestHandler = async (req, res) => {
-  const posts = await Post.create({ ...req.body });
+  try {
+    const posts = await Post.create({ ...req.body });
 
-  return res
-    .status(200)
-    .json({ message: 'Post created successful', data: posts });
+    return res
+      .status(200)
+      .json({ message: EPostMessages.CREATED, data: posts });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: EPostMessages.ERROR, data: error.message });
+  }
 };
 
 export const deletePost: RequestHandler = async (req, res) => {
-  const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-  const deletedPost: Post | null = await Post.findByPk(id);
+    const deletedPost: Post | null = await Post.findByPk(id);
 
-  await Post.destroy({ where: { id } });
+    await Post.destroy({ where: { id } });
 
-  return res
-    .status(200)
-    .json({ message: 'Post deleted successful', data: deletedPost });
+    return res
+      .status(200)
+      .json({ message: EPostMessages.DELETED, data: deletedPost });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: EPostMessages.ERROR, data: error.message });
+  }
 };
 
 export const getAllPosts: RequestHandler = async (req, res) => {
-  const allPosts: Post[] = await Post.findAll();
+  try {
+    const allPosts: Post[] = await Post.findAll();
 
-  return res
-    .status(200)
-    .json({ message: 'Posts fetched successfull', data: allPosts });
+    return res
+      .status(200)
+      .json({ message: EPostMessages.FETCHED, data: allPosts });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: EPostMessages.ERROR, data: error.message });
+  }
 };
 
 export const getPostById: RequestHandler = async (req, res) => {
-  const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-  const post: Post = await Post.findByPk(id);
+    const post: Post = await Post.findByPk(id);
 
-  return res
-    .status(200)
-    .json({ message: 'Post fetched successful', data: post });
+    return res
+      .status(200)
+      .json({ message: EPostMessages.FETCHED_SINGLE, data: post });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: EPostMessages.ERROR, data: error.message });
+  }
 };
 
 export const updatePost: RequestHandler = async (req, res) => {
-  const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-  await Post.update({ ...req.body }, { where: { id } });
+    await Post.update({ ...req.body }, { where: { id } });
 
-  const updatedPost: Post = await Post.findByPk(id);
+    const updatedPost: Post = await Post.findByPk(id);
 
-  return res
-    .status(200)
-    .json({ message: 'Post updated successful', data: updatedPost });
+    return res
+      .status(200)
+      .json({ message: EPostMessages.UPDATED, data: updatedPost });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: EPostMessages.ERROR, data: error.message });
+  }
 };
---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+export enum EPostMessages {
+  ERROR = 'An error occurred!',
+  CREATED = 'Post created successful',
+  DELETED = 'Post deleted successful',
+  FETCHED = 'Posts fetched successful',
+  FETCHED_SINGLE = 'Post fetched successful',
+  UPDATED = 'Post updated successful',
+}
